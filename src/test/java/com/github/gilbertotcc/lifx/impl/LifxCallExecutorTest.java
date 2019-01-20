2bd6e60e2f95e4fac9f5dd8af842dc534677d3c5
Gilberto Taccari <gilbertotcc@users.noreply.github.com>
2019-01-20 10:25:41 +0100

    Migrate to Java 11 and Gradle 5
    * Clean project and set .gitignore
    * Add configuration to publish to bintray.com
    * Update CircleCI configuration
    * Remove Maven files

--- src/test/java/com/github/gilbertotcc/lifx/impl/LifxCallExecutorTest.java
+++ src/test/java/com/github/gilbertotcc/lifx/impl/LifxCallExecutorTest.java
@@ -1,6 +1,8 @@
 package com.github.gilbertotcc.lifx.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
@@ -11,31 +13,27 @@ import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Answers;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import retrofit2.Call;
 import retrofit2.Response;
 
-@RunWith(MockitoJUnitRunner.class)
 public class LifxCallExecutorTest {
 
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private Call<String> callMock;
-
     @Test
+    @SuppressWarnings("unchecked")
     public void getResultShouldSuccess() throws IOException {
+        final var callMock = mock(Call.class, RETURNS_DEEP_STUBS);
         Response<String> response = Response.success("ok");
         when(callMock.execute()).thenReturn(response);
 
-        String okResponse = LifxCallExecutor.of(callMock).getResponse();
+        final var okResponse = LifxCallExecutor.of(callMock).getResponse();
 
         assertEquals("ok", okResponse);
     }
 
     @Test(expected = LifxRemoteException.class)
+    @SuppressWarnings("unchecked")
     public void getResultShouldFailForIOException() throws IOException {
+        final var callMock = mock(Call.class, RETURNS_DEEP_STUBS);
         Request request = new Request.Builder().get().url("http://localhost:8080/test").build();
         when(callMock.execute()).thenThrow(new IOException("error"));
         when(callMock.request()).thenReturn(request);
@@ -43,7 +41,9 @@ public class LifxCallExecutorTest {
     }
 
     @Test(expected = LifxRemoteException.class)
+    @SuppressWarnings("unchecked")
     public void getResultShouldFailForNotSuccessfulResponse() throws IOException {
+        final var callMock = mock(Call.class, RETURNS_DEEP_STUBS);
         String errorResponseBody = TestUtils.loadJsonFromFile("/json/response_body/error_response.json");
         Response<String> response = Response.error(404, ResponseBody.create(MediaType.parse("application/json"), errorResponseBody));
         when(callMock.execute()).thenReturn(response);
