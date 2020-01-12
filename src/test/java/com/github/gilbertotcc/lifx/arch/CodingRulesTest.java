a767bd826def840f1b070ba15db6913e54a1f1cf
Gilberto Taccari <gilbertotcc@users.noreply.github.com>
2020-01-12 15:03:37 +0100

    Fix SonarCloud issues
    * Document deprecations
    * Update Gradle build file
    * Improve code coverage

--- src/test/java/com/github/gilbertotcc/lifx/arch/CodingRulesTest.java
+++ src/test/java/com/github/gilbertotcc/lifx/arch/CodingRulesTest.java
@@ -4,11 +4,12 @@ import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-@AnalyzeClasses(packages = "com.github.gilbertotcc.lifx")
+@AnalyzeClasses(packages = "com.github.gilbertotcc.lifx", importOptions = ImportOption.DoNotIncludeTests.class)
 class CodingRulesTest { //NOSONAR
 
   @ArchTest
